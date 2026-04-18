3ff04f919d00d28396734acec9c9765b077f1633
sanikanair-14 <sanika.nair@somaiya.edu>
2026-04-18 12:10:30 +0530

    Remove backend as submodule

--- backend/routes/chatRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleChatQuery } = require("../controllers/chatController");
-
-// This route does not need 'protect' middleware because anyone on the landing page can chat!
-router.post("/", handleChatQuery);
-
-module.exports = router;
--- backend/routes/recommendationRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRecommendations } = require("../controllers/recommendationController");
-
-router.post("/", getRecommendations);
-
-module.exports = router;
